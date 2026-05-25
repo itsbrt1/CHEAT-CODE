c4af83d479aa3787f25ecb5d2bece6c0a261190c
itsBRT <157130358+itsbrt1@users.noreply.github.com>
2026-05-25 17:39:34 +0545

    Add files via upload

---
+++ backend/submissions/4597ffe0-0df9-4d81-92a8-f64c71dd16a0.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
+
+
+total = num1 + num2
+
+
+print(f"The sum is: {total}")
---
+++ backend/submissions/7d590e01-d3bc-4107-a27e-0a3b815b0280.py
@@ -0,0 +1,2 @@
+for (n):
+    print n*n
